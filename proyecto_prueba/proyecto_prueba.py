ee8efc1816185457174109aed73a55834f4cff33
Martin Avila <mavila@cdainfo.com>
2024-12-19 19:48:22 -0300

    Comenzando con FrontEnd

--- proyecto_prueba/proyecto_prueba.py
+++ proyecto_prueba/proyecto_prueba.py
@@ -4,36 +4,11 @@ import reflex as rx
 
 from rxconfig import config
 
-
 class State(rx.State):
-    """The app state."""
-
-    ...
-
+    pass
 
 def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!!!!!", size="9"),
-            rx.text(
-                "Get started by growing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-        rx.logo(),
-    )
-
+    return rx.text("Hola Reflex.")
 
 app = rx.App()
 app.add_page(index)
