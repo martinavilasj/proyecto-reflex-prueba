46a815e9ed718e6c62091e841c978ae54e7a5d9e
Martin Avila <mavila@cdainfo.com>
2024-12-20 14:06:09 -0300

    Hola Reflex

--- proyecto_prueba/proyecto_prueba.py
+++ proyecto_prueba/proyecto_prueba.py
@@ -8,7 +8,7 @@ class State(rx.State):
     pass
 
 def index() -> rx.Component:
-    return rx.text("Hola Reflex.", color="blue")
+    return rx.text("Hola Reflex!!.", color="blue")
 
 app = rx.App()
 app.add_page(index)
