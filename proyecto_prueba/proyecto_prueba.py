b006fa230c2ea0180db30b669ffc3601afbd4e93
Martin Avila <mavila@cdainfo.com>
2024-12-20 09:55:48 -0300

    Color blue

--- proyecto_prueba/proyecto_prueba.py
+++ proyecto_prueba/proyecto_prueba.py
@@ -8,7 +8,7 @@ class State(rx.State):
     pass
 
 def index() -> rx.Component:
-    return rx.text("Hola Reflex.")
+    return rx.text("Hola Reflex.", color="blue")
 
 app = rx.App()
 app.add_page(index)
