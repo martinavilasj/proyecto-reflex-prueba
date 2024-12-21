dfa7f07035e3a755e599e5765ff5d7685f8926cb
Martin Avila <mavila@cdainfo.com>
2024-12-21 11:35:43 -0300

    Tab

--- proyecto_prueba/proyecto_prueba.py
+++ proyecto_prueba/proyecto_prueba.py
@@ -8,9 +8,36 @@ class State(rx.State):
     pass
     
 def index() -> rx.Component:
-    return rx.box(
-        rx.text("Hola Reflex", color="red"),
-        rx.button("Click me.", color_scheme="grass", variant="soft")
+    return rx.center(
+        rx.tabs.root(
+            rx.tabs.list(
+                rx.tabs.trigger("Clientes", value="clientes"),
+                rx.tabs.trigger("Planes", value="planes"),
+                rx.tabs.trigger("Finanzas", value="finanzas"),
+            ),
+            rx.tabs.content(
+                rx.text("Acá van los clientes xd"),
+                rx.accordion.root(
+                    rx.accordion.item(
+                        header="Nuevo cliente",
+                        content="Acá se agregaria un cliente",
+                        ),
+                    collapsible=True,
+                    width=300px,
+                    type="single",
+                    variant="soft",
+                ),
+                value="clientes",
+            ),
+            rx.tabs.content(
+                rx.text("Acá van los planes xd"),
+                value="planes",
+            ),
+            rx.tabs.content(
+                rx.text("Acá van las finanzas xd"),
+                value="finanzas",
+            ),
+        )
     )
 
 app = rx.App()
