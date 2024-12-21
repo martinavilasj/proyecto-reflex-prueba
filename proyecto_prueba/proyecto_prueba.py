d318b3537ee93497bd6c5b0bd727f98fab7a3246
Martin Avila <mavila@cdainfo.com>
2024-12-21 11:07:12 -0300

    Box

--- proyecto_prueba/proyecto_prueba.py
+++ proyecto_prueba/proyecto_prueba.py
@@ -6,13 +6,12 @@ from rxconfig import config
 
 class State(rx.State):
     pass
-
-def prueba():
-    rx.text("Hola Reflex", color="red")
-    rx.button("Click me.", color_scheme="grass", variant="soft")
-
+    
 def index() -> rx.Component:
-    return prueba()
+    return rx.box(
+        rx.text("Hola Reflex", color="red"),
+        rx.button("Click me.", color_scheme="grass", variant="soft")
+    )
 
 app = rx.App()
 app.add_page(index)
