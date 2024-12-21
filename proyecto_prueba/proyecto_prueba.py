2cd3411a0ac86b97f2bba3c34023ac31b65479d2
Martin Avila <mavila@cdainfo.com>
2024-12-21 11:02:49 -0300

    Hola Reflex

--- proyecto_prueba/proyecto_prueba.py
+++ proyecto_prueba/proyecto_prueba.py
@@ -7,8 +7,12 @@ from rxconfig import config
 class State(rx.State):
     pass
 
+def prueba():
+    rx.text("Hola Reflex", color="red")
+    rx.button("Click me.", color_scheme="grass", variant="soft")
+
 def index() -> rx.Component:
-    return rx.text("Hola Reflex!!.", color="blue")
+    return prueba()
 
 app = rx.App()
 app.add_page(index)
